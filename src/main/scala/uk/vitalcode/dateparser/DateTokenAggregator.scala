e29e47c72648a771329050208ca3edfedfbb12e2
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-03-08 22:44:08 +0000

    Refactor date token aggregator test

--- src/main/scala/uk/vitalcode/dateparser/DateTokenAggregator.scala
+++ src/main/scala/uk/vitalcode/dateparser/DateTokenAggregator.scala
@@ -2,9 +2,22 @@ package uk.vitalcode.dateparser
 
 object DateTokenAggregator {
 
+  def indexTokenList(tokens: List[DateToken]) = {
+    tokens.zipWithIndex.map {
+      case (token: Day, index) => token.copy(index = index)
+      case (token: Month, index) => token.copy(index = index)
+      case (token: Year, index) => token.copy(index = index)
+      case (token: Date, index) => token.copy(index = index)
+      case (token: Time, index) => token.copy(index = index)
+      case (token: Range, index) => token.copy(index = index)
+      case (token: TimeRange, index) => token.copy(index = index)
+    }
+  }
+
   def aggregate(list: List[DateToken]): List[DateToken] = {
     list match {
       case Day(d, i) :: Month(m, _) :: Year(y, _) :: tail => Date(d, m, y, i) :: aggregate(tail)
+      case Month(m, i) :: Day(d, _) :: Year(y, _) :: tail => Date(d, m, y, i) :: aggregate(tail)
       case Time(from, i) :: Range(_) :: Time(to, _) :: tail => TimeRange(from, to, i) :: aggregate(tail)
       case Nil => Nil
       case _ =>  list.head :: aggregate(list.tail)
--- src/test/scala/uk/vitalcode/dateparser/DateTokenAggregatorTest.scala
+++ src/test/scala/uk/vitalcode/dateparser/DateTokenAggregatorTest.scala
@@ -1,11 +1,23 @@
 package uk.vitalcode.dateparser
 
 import org.scalatest._
-import uk.vitalcode.dateparser.DateTokenAggregator.aggregate
+import uk.vitalcode.dateparser.DateTokenAggregator.{aggregate, indexTokenList}
 
 class DateTokenAggregatorTest extends FreeSpec with ShouldMatchers {
 
   "Extract date tokens" in {
-    aggregate(Day(12, 0) :: Month(6, 1) :: Year(2017, 2) :: Nil) shouldBe Date(12,6,2017,0) :: Nil
+    Seq(
+
+      (Day(12) :: Month(6) :: Year(2017) :: Nil) -> (Date(12, 6, 2017, 0) :: Nil),
+      (Day(12) :: Day(22) :: Month(6) :: Year(2017) :: Nil) -> (Day(12, 0) :: Date(22, 6, 2017, 1) :: Nil),
+
+      (Month(6) :: Day(12) :: Year(2017) :: Nil) -> (Date(12, 6, 2017, 0) :: Nil),
+      (Month(6) :: Month(12) :: Day(12) :: Year(2017) :: Nil) -> (Month(6, 0) :: Date(12, 12, 2017, 1) :: Nil)
+
+    ).foreach(assert)
+  }
+
+  private def assert(testExpectations: (List[DateToken], List[DateToken])) = {
+    aggregate(indexTokenList(testExpectations._1)) shouldBe testExpectations._2
   }
 }

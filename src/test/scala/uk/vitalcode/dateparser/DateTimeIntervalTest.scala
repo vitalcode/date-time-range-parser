726fea0baad8c7b00f67e7cfe1d618bea79ba668
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-03-09 13:02:12 +0000

    Refactor date time interval test

--- src/test/scala/uk/vitalcode/dateparser/DateTimeIntervalTest.scala
+++ src/test/scala/uk/vitalcode/dateparser/DateTimeIntervalTest.scala
@@ -4,20 +4,36 @@ import org.scalatest._
 
 class DateTimeIntervalTest extends FreeSpec with ShouldMatchers {
 
-  "Parse data time tokens list as a list of date time intervals" in {
-    Seq(
-      (Day(6) :: Month(5) :: Year(2017) :: Nil) -> (DateTimeInterval.from(2017, 5, 6, 0, 0) :: Nil),
-      (Month(9) :: Day(10) :: Year(2017) :: Time(12) :: Nil) -> (DateTimeInterval.from(2017, 9, 10, 12, 0) :: Nil)
-    ).foreach(assert)
+  "parse data time tokens list as a list of date time intervals" - {
+
+    "single date (day, month, year tokens)" - {
+
+      "no time tokens" in {
+        assert(List(Day(6), Month(5), Year(2017)) -> List(
+          DateTimeInterval.from(2017, 5, 6, 0, 0)
+        ))
+      }
+      "single time token" in {
+        assert(List(Day(6), Month(5), Year(2017), Time(12)) -> List(
+          DateTimeInterval.from(2017, 5, 6, 12, 0)
+        ))
+      }
+      "multiple time token" in {
+        assert(List(Day(6), Month(5), Year(2017), Time(12), Time(14)) -> List(
+          DateTimeInterval.from(2017, 5, 6, 12, 0),
+          DateTimeInterval.from(2017, 5, 6, 14, 0)
+        ))
+      }
+    }
+
+    "single date (month, day, year tokens)" in {
+      assert(List(Month(9), Day(10), Year(2017), Time(12)) -> List(
+        DateTimeInterval.from(2017, 9, 10, 12, 0)
+      ))
+    }
   }
 
   private def assert(testExpectations: (List[DateToken], List[DateTimeInterval])) = {
     DateTimeInterval.of(testExpectations._1) shouldBe testExpectations._2
   }
 }
-
-
-//"date with four letter month abbreviation + 2 different from time" in {
-//assertDate("Sept. 10, 2016 11:00am, 3:00pm",
-//LocalDateTime.of(2016, Month.SEPTEMBER, 10, 11, 0),
-//LocalDateTime.of(2016, Month.SEPTEMBER, 10, 15, 0))

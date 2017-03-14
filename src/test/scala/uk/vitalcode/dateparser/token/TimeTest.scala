fffababf2fd5bb7f487ea8168bc3242352bd5cc2
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-03-14 08:42:42 +0000

    Refactor token tests

--- src/test/scala/uk/vitalcode/dateparser/token/TimeTest.scala
+++ src/test/scala/uk/vitalcode/dateparser/token/TimeTest.scala
@@ -5,53 +5,53 @@ class TimeTest extends TokenTest {
   "parsing string containing valid time" - {
 
     "create TimeToken for 24 hours time format string" in {
-      "20:05:30" should beToken[Time](Time(20, 5, 30))
-      "20:05:30" should beToken[Time](Time(20, 5, 30))
-      "20:05" should beToken[Time](Time(20, 5))
-      "20:5" should beToken[Time](Time(20, 5))
-      "5:5" should beToken[Time](Time(5, 5))
-      "05:5" should beToken[Time](Time(5, 5))
-      "05:05" should beToken[Time](Time(5, 5))
-      "5:5:20" should beToken[Time](Time(5, 5, 20))
-      "5:5:05" should beToken[Time](Time(5, 5, 5))
-      "5:5:9" should beToken[Time](Time(5, 5, 9))
+      "20:05:30" should beToken(Time(20, 5, 30))
+      "20:05:30" should beToken(Time(20, 5, 30))
+      "20:05" should beToken(Time(20, 5))
+      "20:5" should beToken(Time(20, 5))
+      "5:5" should beToken(Time(5, 5))
+      "05:5" should beToken(Time(5, 5))
+      "05:05" should beToken(Time(5, 5))
+      "5:5:20" should beToken(Time(5, 5, 20))
+      "5:5:05" should beToken(Time(5, 5, 5))
+      "5:5:9" should beToken(Time(5, 5, 9))
     }
 
     "create TimeToken for 24 hours dot separated time format string" in {
 
-      "20.05.30" should beToken[Time](Time(20, 5, 30))
-      "5.5" should beToken[Time](Time(5, 5))
-      "5.5.05" should beToken[Time](Time(5, 5, 5))
+      "20.05.30" should beToken(Time(20, 5, 30))
+      "5.5" should beToken(Time(5, 5))
+      "5.5.05" should beToken(Time(5, 5, 5))
     }
 
     "create TimeToken for 12 hours time format string" in {
 
-      "08:05PM" should beToken[Time](Time(20, 5))
-      "08:05:25PM" should beToken[Time](Time(20, 5, 25))
-      "8:5:5PM" should beToken[Time](Time(20, 5, 5))
-      "08:5:5PM" should beToken[Time](Time(20, 5, 5))
-      "08:05:5PM" should beToken[Time](Time(20, 5, 5))
-      "08:5:05PM" should beToken[Time](Time(20, 5, 5))
-      "08:5:05AM" should beToken[Time](Time(8, 5, 5))
-      "12:0AM" should beToken[Time](Time(0, 0))
-      "12:00PM" should beToken[Time](Time(12, 0))
-      "08:05:25 PM" should beToken[Time](Time(20, 5, 25))
-      "08:05:25 pm" should beToken[Time](Time(20, 5, 25))
-      "8:5:5am" should beToken[Time](Time(8, 5, 5))
-      "11pm" should beToken[Time](Time(23, 0))
-      "6 AM" should beToken[Time](Time(6, 0))
+      "08:05PM" should beToken(Time(20, 5))
+      "08:05:25PM" should beToken(Time(20, 5, 25))
+      "8:5:5PM" should beToken(Time(20, 5, 5))
+      "08:5:5PM" should beToken(Time(20, 5, 5))
+      "08:05:5PM" should beToken(Time(20, 5, 5))
+      "08:5:05PM" should beToken(Time(20, 5, 5))
+      "08:5:05AM" should beToken(Time(8, 5, 5))
+      "12:0AM" should beToken(Time(0, 0))
+      "12:00PM" should beToken(Time(12, 0))
+      "08:05:25 PM" should beToken(Time(20, 5, 25))
+      "08:05:25 pm" should beToken(Time(20, 5, 25))
+      "8:5:5am" should beToken(Time(8, 5, 5))
+      "11pm" should beToken(Time(23, 0))
+      "6 AM" should beToken(Time(6, 0))
     }
 
     "create TimeToken for 12 hours dot separated time format string" in {
 
-      "8.05.30 pm" should beToken[Time](Time(20, 5, 30))
-      "5.5 am" should beToken[Time](Time(5, 5))
-      "5.5.05PM" should beToken[Time](Time(17, 5, 5))
+      "8.05.30 pm" should beToken(Time(20, 5, 30))
+      "5.5 am" should beToken(Time(5, 5))
+      "5.5.05PM" should beToken(Time(17, 5, 5))
     }
 
     "create TimeToken for time strings containing non-time related text" in {
 
-      "Time:5.45pm" should beToken[Time](Time(17, 45))
+      "Time:5.45pm" should beToken(Time(17, 45))
     }
   }
   "parsing strings that do not contain valid time" - {
--- src/test/scala/uk/vitalcode/dateparser/token/YearTest.scala
+++ src/test/scala/uk/vitalcode/dateparser/token/YearTest.scala
@@ -5,20 +5,20 @@ class YearTest extends TokenTest {
   "parsing string containing valid year value" - {
 
     "should be a number in 1900-2099 interval" in {
-      "1900" should beToken[Year](Year(1900))
-      "2099" should beToken[Year](Year(2099))
-      "2017" should beToken[Year](Year(2017))
+      "1900" should beToken(Year(1900))
+      "2099" should beToken(Year(2099))
+      "2017" should beToken(Year(2017))
 
-      "2100" shouldNot beToken[Year](Year(2100))
-      "1801" shouldNot beToken[Year](Year(1801))
+      "2100" shouldNot beToken[Year]
+      "1801" shouldNot beToken[Year]
       "12" shouldNot beToken[Year]
     }
 
     "may contain other leading or trailing characters" in {
-      "leading2018" should beToken[Year](Year(2018))
-      "2019bc" should beToken[Year](Year(2019))
-      "2020AD" should beToken[Year](Year(2020))
-      "leading2000trailing" should beToken[Year](Year(2000))
+      "leading2018" should beToken(Year(2018))
+      "2019bc" should beToken(Year(2019))
+      "2020AD" should beToken(Year(2020))
+      "leading2000trailing" should beToken(Year(2000))
     }
   }
 

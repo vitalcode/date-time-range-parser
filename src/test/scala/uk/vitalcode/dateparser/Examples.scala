717b73ed12447c528ca84728ba2cec4440d7e032
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-03-20 21:43:17 +0000

    Refactor  examples

--- src/test/scala/uk/vitalcode/dateparser/Examples.scala
+++ src/test/scala/uk/vitalcode/dateparser/Examples.scala
@@ -4,32 +4,27 @@ import java.time.LocalDateTime
 
 class Examples extends ExamplesSupport {
 
-  override protected val currentDate = LocalDateTime.of(2017, 5, 6, 0, 0)
+  override protected val currentDate: LocalDateTime = LocalDateTime.of(2017, 5, 6, 0, 0)
 
-  override protected val examples = List(
-    "6 May 2017" -> List(
-      DateTimeInterval.from(2017, 5, 6)
-    ),
-    "Saturday, January 9th 2017 from 3:00 PM" -> List(
-      DateTimeInterval.from(2017, 1, 9, 15, 0)
-    ),
-    "Thursday, March 17th 2017 from 7:00 PM to 8:30 PM" -> List(
-      DateTimeInterval.from(2017, 3, 17, 19, 0).to(2017, 3, 17, 20, 30)
-    ),
-    "Sept. 10, 2017 12:00pm" -> List(
-      DateTimeInterval.from(2017, 9, 10, 12, 0)
-    ),
-    "Sept. 10, 2017 11:00am, 3:00pm" -> List(
-      DateTimeInterval.from(2017, 9, 10, 11, 0),
-      DateTimeInterval.from(2017, 9, 10, 15, 0)
-    ),
-    "Fri 24 Jun 6:45pm (doors) | 11pm (curfew)" -> List(
-      DateTimeInterval.from(2017, 6, 24, 18, 45).to(2017, 6, 24, 23, 0)
-    ),
-    "February, 23 17:25 - 18:05" -> List(
-      DateTimeInterval.from(2018, 2, 23, 17, 25).to(2018, 2, 23, 18, 5)
-    )
-  )
+  "6 May 2017" in expected(
+    DateTimeInterval.from(2017, 5, 6))
 
-  run()
+  "Saturday, January 9th 2017 from 3:00 PM" in expected(
+    DateTimeInterval.from(2017, 1, 9, 15, 0))
+
+  "Thursday, March 17th 2017 from 7:00 PM to 8:30 PM" in expected(
+    DateTimeInterval.from(2017, 3, 17, 19, 0).to(2017, 3, 17, 20, 30))
+
+  "Sept. 10, 2017 12:00pm" in expected(
+    DateTimeInterval.from(2017, 9, 10, 12, 0))
+
+  "Sept. 10, 2017 11:00am, 3:00pm" in expected(
+    DateTimeInterval.from(2017, 9, 10, 11, 0),
+    DateTimeInterval.from(2017, 9, 10, 15, 0))
+
+  "Fri 24 Jun 6:45pm (doors) | 11pm (curfew)" in expected(
+    DateTimeInterval.from(2017, 6, 24, 18, 45).to(2017, 6, 24, 23, 0))
+
+  "February, 23 17:25 - 18:05" in expected(
+    DateTimeInterval.from(2018, 2, 23, 17, 25).to(2018, 2, 23, 18, 5))
 }
--- src/test/scala/uk/vitalcode/dateparser/ExamplesSupport.scala
+++ src/test/scala/uk/vitalcode/dateparser/ExamplesSupport.scala
@@ -2,22 +2,19 @@ package uk.vitalcode.dateparser
 
 import java.time.LocalDateTime
 
-import org.scalamock.scalatest.MockFactory
+import org.scalatest._
 import org.scalatest.matchers.{MatchResult, Matcher}
-import org.scalatest.{FreeSpec, _}
 
 
-trait ExamplesSupport extends FreeSpec with Matchers with MockFactory {
+trait ExamplesSupport extends fixture.FreeSpec with fixture.TestDataFixture with Matchers {
 
   protected val currentDate: LocalDateTime
-
-  protected val examples: List[(String, List[DateTimeInterval])]
 
   private val dateTimeProvider = new DateTimeProvider {
     override def now: LocalDateTime = currentDate
   }
 
-  protected def parseAs(right: List[DateTimeInterval]): Matcher[String] = new Matcher[String] {
+  private def parseAs(right: List[DateTimeInterval]): Matcher[String] = new Matcher[String] {
     def apply(left: String): MatchResult = {
       val actual = DateTimeInterval.of(left, dateTimeProvider)
       MatchResult(
@@ -28,15 +25,6 @@ trait ExamplesSupport extends FreeSpec with Matchers with MockFactory {
     }
   }
 
-  protected def run() = {
-    "should parse text as date time intervals" - {
-      examples.foreach {
-        case (textToParse, expectation) => {
-          textToParse in {
-            textToParse should parseAs(expectation)
-          }
-        }
-      }
-    }
-  }
+  protected def expected(expectation: DateTimeInterval*) =
+    (text: FixtureParam) => text.name should parseAs(expectation.toList)
 }
